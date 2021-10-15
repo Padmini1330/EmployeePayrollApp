d77d2c348bfe6d103026aa986a66f6f1033599c0
padmini v <padmini.v@perfios.com>
2021-10-15 15:08:29 +0530

    REFACTORED: Added update function

--- js/employee_home.js
+++ js/employee_home.js
@@ -13,52 +13,50 @@ const getEmployeePayrollDataFromStorage = () => {
 
 const createInnerHtml = () => {
     const headerHtml ="<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-    if (employeePayrollList.length == 0) {
+    let innerHtml = `${headerHtml}`;
+    if (employeePayrollList.length == 0) 
+    {
         return;
     }
-    let innerHtml = `${headerHtml}`;
-    
-    for (let employeePayrollData of employeePayrollList) {
-        console.log(employeePayrollData._profilePicture);
+    for (let employeePayrollData of employeePayrollList) 
+    {
         innerHtml = `${innerHtml}
         <tr>
-            <td><img class="profile" src="${employeePayrollData._profilePicture}"></td>
+            <td><img class="profile" alt="" src="${employeePayrollData._profilePicture}"></td>
             <td>${employeePayrollData._name}</td>
             <td>${employeePayrollData._gender}</td>
             <td>${getDepartmentHtml(employeePayrollData._departments)}</td>
             <td>${employeePayrollData._salary}</td>
             <td>${stringifyDate(employeePayrollData._startDate)}</td>
             <td>
-                <img id="${employeePayrollData._id}" onclick="remove(this)"  src="../assets/icons/delete-black-18dp.svg">
-                <img id="${employeePayrollData._id}" onclick="update(this)"  src="../assets/icons/create-black-18dp.svg">
+                <img id="${employeePayrollData._id}" onclick="remove(this)" alt="delete" src="..//assets/icons/delete-black-18dp.svg">
+                <img id="${employeePayrollData._id}" onclick="update(this)" alt="edit" src="..//assets/icons/create-black-18dp.svg">
             </td>
         </tr>
         `;
     }
     document.querySelector("#display").innerHTML = innerHtml;
 };
-                    
+
 const getDepartmentHtml = (departmentList) => {
     let departmentHtml = "";
-    for (let department of departmentList) {
-        departmentHtml = `${departmentHtml} <div class="dept-label">${department}</div>`;
+    if (departmentList) 
+    {
+        for (let department of departmentList) 
+        {
+            departmentHtml = `${departmentHtml} <div class="dept-label">${department}</div>`;
+        }
     }
     return departmentHtml;
-};     
-                                      
+};
+
 const remove = (node) => {
     let employeePayrollData = employeePayrollList.find(employeeData => employeeData._id == node.id);
-    if (!employeePayrollData)
-    {
-        return;
-    } 
-    const index = employeePayrollList.map(employeeData => employeeData._id)
-                                    .indexOf(employeePayrollData._id);
-                                    
+    if (!employeePayrollData) return;
+    const index = employeePayrollList.map(employeeData => employeeData._id).indexOf(employeePayrollData._id);
     employeePayrollList.splice(index, 1);
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
     document.querySelector(".emp-count").textContent = employeePayrollList.length;
-    console.log(employeePayrollList.length);
     createInnerHtml();
 };
 
